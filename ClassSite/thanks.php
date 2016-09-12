93a0f4cd3df59d836cb5becfc0980d22ddc1046e
Lingren <hlingren@thomas.butler.edu>
2016-09-12 14:34:47 -0400

    init

---
+++ ClassSite/thanks.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>CME Capstone Event</title>
+				<link href="v1_styles.css" rel="stylesheet" type="text/css"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
+	<body>
+		
+			<a href="index.html" id="logoLink"><img src="../Images/CapstoneLogoSM.svg" alt="Capstone Logo" width="157" height="58"/></a>
+			<nav>
+				<ul>
+					<li><a href="about.html">About</a></li>
+					<li><a href="schedule.html">Schedule</a></li>
+					<li><a href="projects.html">Projects</a></li>
+					<li><a href="students.html">Students</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+			</nav>
+			
+			<header><h1>Contact Us</h1></header>
+			<article>
+				<p>Thank you for your comments. We will get back to you as soon as possible.</p>
+		
+			</article>
+			
+			
+		<footer>
+			<p>Creative Media and Entertainment <br/>
+				College of Communication <br/>
+				Butler University
+			</p>
+		</footer>
+		
+	</body>
+</html>
